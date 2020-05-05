a14cc31ed098d9fe79ebb3346451c1d3378fd7b8
Eddie Galvez <eddie@tibco.com>
2020-05-05 14:46:53 -0400

    initial commit, escaped identifier mapper

---
+++ components/escapedidentifiermapper/README.md
@@ -0,0 +1,13 @@
+# Escaped Identifier Mapper Operator
+
+This project provides a TIBCO StreamBase Operator that simply maps its input to its output, but renaming any field name that is escaped (those with "#...") according to a simple prefix configured in the properties. 
+
+
+# Gotchas
+
+It doesn't handle fields of type FUNCTION, and has no special processing for CAPTURE fields
+
+
+# Changelog
+
+1.0		Initial version
---
+++ components/escapedidentifiermapper/src/main/java/com/tibco/ep/sb/contrib/escapedidentifiermapper/EscapedFieldNameCleanerBeanInfo.java
@@ -0,0 +1,26 @@
+package com.tibco.ep.sb.contrib.escapedidentifiermapper;
+
+import java.beans.*;
+
+import com.streambase.sb.operator.parameter.*;
+
+/**
+* A BeanInfo class controls what properties are exposed, add 
+* metadata about properties (such as which properties are optional), and access 
+* special types of properties that can't be automatically derived via reflection. 
+* If a BeanInfo class is present, only the properties explicitly declared in
+* this class will be exposed by StreamBase.
+*/
+public class EscapedFieldNameCleanerBeanInfo extends SBSimpleBeanInfo {
+
+	/*
+	* The order of properties below determines the order they are displayed within
+	* the StreamBase Studio property view. 
+	*/
+	public SBPropertyDescriptor[] getPropertyDescriptorsChecked() throws IntrospectionException {
+		SBPropertyDescriptor[] p = { new SBPropertyDescriptor("prefix", EscapedFieldNameCleaner.class)
+				.displayName("Prefix to use").description(""), };
+		return p;
+	}
+
+}
