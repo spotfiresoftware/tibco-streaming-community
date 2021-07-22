16b72b438fa34be30c5859f81f1de57588c73be0
oblaufus <oblaufus@tibco.com>
2021-07-22 14:50:08 -0400

    add motor as a sensor type

--- components/mindstorms-ev3/src/main/java/com/example/ev3/SensorTypeEnum.java
+++ components/mindstorms-ev3/src/main/java/com/example/ev3/SensorTypeEnum.java
@@ -1,7 +1,7 @@
 package com.example.ev3;
 
 public enum SensorTypeEnum {
-	TOUCH("Touch"), COLOR("Color"), ULTRA("Ultrasonic"), GYRO("Gyroscope"), IR("Infrared"), NONE("None");
+	TOUCH("Touch"), COLOR("Color"), ULTRA("Ultrasonic"), GYRO("Gyroscope"), IR("Infrared"), MOTOR("Motor"), NONE("None");
 
 	private final String rep;
 
